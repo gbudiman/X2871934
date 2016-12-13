0f4587c80dfb4cb5b0343c013838a2c0e4ed1c21
Gloria Budiman <wahyu.g@gmail.com>
2016-12-12 18:19:41 -0800

    Step2 implementation.

--- app/assets/javascripts/fetch_queryset.js
+++ app/assets/javascripts/fetch_queryset.js
@@ -6,10 +6,12 @@ function fetch_queryset() {
         $.each(data, function(name, x) {
           var s = $('<div></div>')
                     .addClass('col-xs-12 col-sm-6 col-md-4 col-lg-2')
+                    .addClass('img-padded')
+                    .attr('data-queryset-id', name)
                     .append($('<img></img>')
                               .attr('alt', name)
                               .attr('src', 'photo_placeholder.jpg')
-                              .addClass('img-rounded img-padded')
+                              .addClass('img-rounded')
                               .attr('width', 128)
                               .attr('height', 128));
 
@@ -21,6 +23,21 @@ function fetch_queryset() {
     );
   }
 
+  var attach_step2_click = function() {
+    return new Promise(
+      function(resolve, reject) {
+        $('[data-queryset-id').each(function() {
+          $(this).on('click', function() {
+            console.log($(this).attr('data-queryset-id'));
+          })
+        })
+
+        resolve();
+      }
+    )
+    
+  }
+
   $('#step2-placeholder').hide();
   $('#step2-loading').show();
   $.get({
@@ -30,6 +47,7 @@ function fetch_queryset() {
     $('#step2-loading').hide();
     render_step2(data).then(function() {
       maximize_height($('#step2'), 16);
+      attach_step2_click();
     })
   })
 }
